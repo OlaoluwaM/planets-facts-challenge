1976804752b412eaf4d2006404993f98723dc7a8
Olaoluwa Mustapha <olaolumustapha@protonmail.ch>
2021-07-03 12:05:31 +0100

    Initialize project using Create React App

---
+++ src/context/theme.js
@@ -0,0 +1,3 @@
+const themeContext = {};
+
+export default themeContext;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import App from './pages/App';
+import reportWebVitals from './reportWebVitals';
+
+import { render } from 'react-dom';
+import { StrictMode } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.scss';
+
+render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
